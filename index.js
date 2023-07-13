e10d5d84e7bc210e7ed1178c89b963524edb82d7
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-07-13 23:07:06 +0200

    call to action mobile

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const emailInput = document.getElementById("email");
-const email = emailInput.value;
-const formButton = document.querySelector(".submit-form");
+var emailInput = document.getElementById("email");
+var emailValue = emailInput.value;
+var form = document.getElementById("main-form");
+var formButton = document.querySelector(".submit-form");
 
 function validateEmail(email) {
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
   // Regular expression pattern for email validation
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   // Check if the email matches the pattern
   return emailPattern.test(email);
 }
 
-formButton.addEventListener("click", (event) => {
-  event.preventDefault();
-
+form.addEventListener("submit", (event) => {
+  // prevent default behaviour
+  console.log(emailInput);
+  console.log(emailValue);
   if (validateEmail(email)) {
     // email is valid
-    console.log("email is valid");
+    alert("email is valid");
   } else {
     // email is invalid
-    console.log("Email is invalid");
+    alert("Email is invalid");
   }
+  event.preventDefault();
 });
